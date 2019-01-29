2a4eec5a7a099ea7ab3a747f28663a02af59947a
Ranjith Manickam <mranjithkumar@mranjithkumar-macbookpro.roam.corp.google.com>
2019-01-29 18:35:50 +0530

    Initial commit.

--- web/src/main/java/web/model/RequestInfo.java
+++ web/src/main/java/web/model/RequestInfo.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.io.Serializable;
+
 /**
  * author: Ranjith Manickam @ 28 Jan' 2019.
  */
@@ -12,7 +14,7 @@ import lombok.ToString;
 @Setter
 @Builder
 @ToString
-public class RequestInfo extends AbstractEntry {
+public class RequestInfo implements Serializable {
     private static final long serialVersionUID = -3965961885659864153L;
 
     private String requestId;
